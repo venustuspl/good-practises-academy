e7432e160b45fd4a40b1b26772dcedcd8fce3a5e
venustuspl <venustus.pl@gmail.com>
2023-06-07 20:44:32 +0200

    AmountValueObject refactor

--- src/main/java/pl/praktycznajava/module3/valueobjects/challenge2/CurrencyConverter.java
+++ src/main/java/pl/praktycznajava/module3/valueobjects/challenge2/CurrencyConverter.java
@@ -4,9 +4,6 @@ import pl.praktycznajava.module3.valueobjects.challenge2.model.Currency;
 
 import java.math.BigDecimal;
 
-import static pl.praktycznajava.module3.valueobjects.challenge2.OrderService.FREE_SHIPPING_THRESHOLD_AMOUNT;
-import static pl.praktycznajava.module3.valueobjects.challenge2.OrderService.FREE_SHIPPING_THRESHOLD_CURRENCY;
-
 public class CurrencyConverter implements Converter{
     @Override
     public BigDecimal convertTo(BigDecimal amount, Currency fromCurrency, Currency toCurrency) {
--- src/main/java/pl/praktycznajava/module3/valueobjects/challenge2/model/Amount.java
+++ src/main/java/pl/praktycznajava/module3/valueobjects/challenge2/model/Amount.java
@@ -3,7 +3,6 @@ package pl.praktycznajava.module3.valueobjects.challenge2.model;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import pl.praktycznajava.module3.valueobjects.challenge2.Converter;
 
@@ -11,7 +10,6 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 @Getter
-@Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor(staticName = "of")
 public class Amount {
@@ -19,11 +17,11 @@ public class Amount {
     public static final Currency FREE_SHIPPING_THRESHOLD_CURRENCY = Currency.USD;
     public static final BigDecimal FREE_SHIPPING_THRESHOLD_AMOUNT = BigDecimal.valueOf(100);
 
-    private BigDecimal totalAmount;
-    private Currency currency;
+    private final BigDecimal totalAmount;
+    private final Currency currency;
 
     public boolean hasFreeShipping(Converter converter) {
-        if(currency != FREE_SHIPPING_THRESHOLD_CURRENCY) {
+        if (currency != FREE_SHIPPING_THRESHOLD_CURRENCY) {
             BigDecimal convertedAmount = converter.convertTo(this.totalAmount, this.currency, FREE_SHIPPING_THRESHOLD_CURRENCY);
             return convertedAmount.compareTo(FREE_SHIPPING_THRESHOLD_AMOUNT) > 0;
         } else {
@@ -31,12 +29,13 @@ public class Amount {
         }
     }
 
-    public void addDiscount(Converter converter, BigDecimal discount, Currency discountCurrency) {
-        this.totalAmount = this.totalAmount.subtract(converter.convertTo(discount, discountCurrency, this.currency));
+    public Amount addDiscount(Converter converter, BigDecimal discount, Currency discountCurrency) {
+        return Amount.of(this.totalAmount.subtract(converter.convertTo(discount, discountCurrency, this.currency)), this.currency);
     }
 
-    public void addPercentageDiscount(int percentageDiscount) {
-        this.totalAmount = totalAmount.subtract(totalAmount.multiply(BigDecimal.valueOf(percentageDiscount))
-                .divide(BigDecimal.valueOf(MAX_PERCENT), RoundingMode.HALF_UP));
+    public Amount addPercentageDiscount(int percentageDiscount) {
+        return Amount.of(this.totalAmount.subtract(this.totalAmount.multiply(BigDecimal.valueOf(percentageDiscount)).divide(BigDecimal.valueOf(MAX_PERCENT), RoundingMode.HALF_UP)), this.currency);
     }
 }
+
+//Value Objecty nie mają setterów, więc tam @Setter do usunięcia 🙂 No i jeszcze ustawiamy pola jako final w nich. Aby wykonać działanie na value objectcie, po prostu wykonujemy dane działanie i wynik zapisujemy już w nowym obiekcie i jego zwracamy na zewnątrz 🙂
